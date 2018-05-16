2f1b8e70b9deee0e354e371b16cb0a2a0a58d66e
Jesse Nunes <s5081040@bournemouth.ac.uk>
2018-05-16 17:13:18 +0100

    VER 0.5

---
+++ Assets/catcherBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class catcherBehaviour : MonoBehaviour {
+
+    public GameObject Falling_Hazard;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == ("ball_02"))
+        {
+            Destroy(Falling_Hazard);
+
+        }
+    }
+}
+

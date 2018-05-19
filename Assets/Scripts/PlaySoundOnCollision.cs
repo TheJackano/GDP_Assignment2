5dc2c9b9157af2aaaa841d211203a1931b4ad353
Jesse Nunes <s5081040@bournemouth.ac.uk>
2018-05-19 10:30:28 +0100

    Created README file for new users; Added Ball Bounce sound effect and created a script that will trigger the sound effect on the object's collision with another object

---
+++ Assets/Scripts/PlaySoundOnCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlaySoundOnCollision : MonoBehaviour {
+
+    // Use this for initialization
+    AudioSource BallBounceFile;
+
+	void Start () {
+        BallBounceFile = GetComponent<AudioSource>();
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        BallBounceFile.Play();
+    }
+}
---
+++ README.txt
@@ -0,0 +1,3 @@
+To open the Rube Goldberg Machine I created click on the "Assets" folder. 
+In that folder click on the "GDP_RubeGoldbergMachineMenu.unity" file to open the menu.
+From that menu you can press the "start" button to start the machine and view it running.
